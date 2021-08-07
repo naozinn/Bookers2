b913c64c6552d76d0ea1661b765daf80f45abc2b
naozinn <naozinn0509@gmail.com>
2021-08-07 12:35:24 +0000

    部分テンプレート適用

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'homes#top'
   get 'home/about' => 'homes#about',as: 'about'
-  devise_for :users
+
   resources :books, only: [:index, :show, :create, :edit, :destroy, :update]
   resources :users, only: [:index, :show, :create, :edit, :update]
 end
